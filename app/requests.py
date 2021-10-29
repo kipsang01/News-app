058c466f9e429785c41f99fc0bc71176b54ca18c
kipsang01 <enock.kipsang@student.moringaschool.com>
2021-10-30 00:17:17 +0300

    Add request to return headlines

--- app/requests.py
+++ app/requests.py
@@ -4,14 +4,16 @@ from newsapi import NewsApiClient
 # Init
 newsapi = NewsApiClient(api_key= Api_Key)
 
-# /v2/top-headlines
-top_headlines = newsapi.get_top_headlines(q='bitcoin')
+def headlines():
+    
+    top_headlines = newsapi.get_top_headlines()
                                         #   sources='bbc-news,the-verge',
                                         #   category='business',
                                         #   language='en',
                                         #   country='us')
+                                        
+    return top_headlines
 
-# /v2/everything
 def all_news():
     all_articles = newsapi.get_everything(q='',
                                         sources='bbc-news,the-verge',
