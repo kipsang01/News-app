a22ab015e0aa26eac69d272340e027bb7fd34da6
kipsang01 <enock.kipsang@student.moringaschool.com>
2021-11-01 15:17:04 +0300

    deployment to heroku

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app
 from flask_script import Manager,Server
-from .config import Config
+from config import Config
 
 SECRET_KEY =Config.SECRET_KEY
 app = create_app('development')
